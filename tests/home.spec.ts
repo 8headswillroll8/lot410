64015d04a92b5d844c1e6abe2cd3320a3e461ad9
8headswillroll8 <lejohansen91@gmail.com>
2026-08-18 21:35:13 +0200

    chore: add project structure and assets

--- tests/home.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("displays LOT410 heading", async ({ page }) => {
-  await page.goto("/");
-
-  await expect(page.getByRole("heading", { name: "LOT410" })).toBeVisible();
-});
