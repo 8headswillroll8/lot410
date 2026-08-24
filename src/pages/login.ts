b92392413c869f6c8d957329ceb64794577baca1
8headswillroll8 <lejohansen91@gmail.com>
2026-08-24 23:22:23 +0200

    feat: add login form validation

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -2,3 +2,51 @@ import "../style.css";
 import { setupMobileMenu } from "../components/mobileMenu";
 
 setupMobileMenu();
+
+const loginForm = document.querySelector<HTMLFormElement>("#login-form");
+const loginEmail = document.querySelector<HTMLInputElement>("#login-email");
+const loginPassword =
+  document.querySelector<HTMLInputElement>("#login-password");
+const loginAlert = document.querySelector<HTMLDivElement>("#login-alert");
+const loginAlertText =
+  document.querySelector<HTMLParagraphElement>("#login-alert-text");
+
+if (
+  !loginForm ||
+  !loginEmail ||
+  !loginPassword ||
+  !loginAlert ||
+  !loginAlertText
+) {
+  throw new Error("Login form elements not found");
+}
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = loginEmail.value.trim().toLowerCase();
+  const password = loginPassword.value.trim();
+
+  if (email === "") {
+    loginAlert.classList.remove("hidden");
+    loginAlert.classList.add("flex");
+    loginAlertText.textContent = "You have to put in an email";
+    return;
+  }
+
+  if (!email.endsWith("@stud.noroff.no")) {
+    loginAlert.classList.remove("hidden");
+    loginAlert.classList.add("flex");
+    loginAlertText.textContent = "You'll need a noroff student email for this";
+    return;
+  }
+
+  if (password === "") {
+    loginAlert.classList.remove("hidden");
+    loginAlert.classList.add("flex");
+    loginAlertText.textContent = "You have to put in a password";
+    return;
+  }
+
+  console.log("ready to log in");
+});
